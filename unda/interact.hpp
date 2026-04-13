8db4ddfcb67d1e3252fc3e5da8433e8a41026c52
young_tea <3h.young.tea@gmail.com>
2026-04-13 17:42:01 +0800

    Add interact class template in interact.hpp

---
+++ unda/interact.hpp
@@ -0,0 +1,83 @@
+#pragma once
+
+#if	__cplusplus <= 202302l
+#error	"我的瓷, 请更新编译器"
+#endif
+
+#include <bits/stdc++.h>
+#include "beatz.hpp"
+#include "loop.hpp"
+
+using namespace std::literals;
+
+namespace unda {
+//	https://music.douban.com/subject/30305164/
+
+template<class ty>
+class	interact {
+public:
+	robj_t<interact<ty>> pre_;
+	sobj_t<interact<ty>> nxt_;
+	uobj_t<ty> val_;
+
+	template <class...args>
+	interact(args&&...arg)
+		: val_(std::forward<args>(arg)...) {}
+
+	~interact(void) noexcept {
+		if (!pre_) {
+			while (nxt_)
+				nxt_->rm();
+
+			return;
+		}
+
+		rm();
+	}
+
+	template <class ref_ty, class...args>
+	void	touch(const ref_ty &ref_this, args&&...arg)
+		pre(&*ref_this == this) {
+		sobj_t<interact<ty>> x(std::forward<args>(arg)...);
+		x->pre_ = ref_this;
+		x->nxt_ = nxt_;
+
+		if (nxt_)
+			nxt_->pre_ = x;
+
+		nxt_ = x;
+	}
+
+	void	rm(void) noexcept
+		pre(pre_) {
+		if (nxt_)
+			nxt_->pre_ = pre_;
+
+		pre_->nxt_ = nxt_;
+	}
+
+	bool	has_ring(void) const noexcept
+		pre(!pre_) {
+		std::map<robj_t<interact<ty>>, bool> book;
+
+		for (robj_t<interact<ty>> x = nxt_; x; x = x->nxt_) {
+			if (book[x])
+				return 1;
+
+			book[x] = 1;
+		}
+
+		return 0;
+	}
+
+	loop<robj_t<interact<ty>>> cast_loop(void) const noexcept
+		pre(!pre_ && !has_ring()) {
+		loop<robj_t<interact<ty>>> taocp;
+
+		for (robj_t<interact<ty>> x = nxt_; x; x = x->nxt_)
+			taocp.emplace(x);
+
+		return taocp;
+	}
+};
+}
